e821743aadac8a9108b4b21c582350dae8706ab8
Ray Myers <raydog87@gmail.com>
2017-05-19 10:18:05 -0700

    WIP on main executable code

--- src/main.js
+++ src/main.js
@@ -1,65 +1,104 @@
-var parser = require('./parser');
-var astHandler = require('./astHandler');
-var formatter = require('./formatter');
-var jsondiffpatch = require('jsondiffpatch');
-var util = require('util');
-var _ = require('lodash');
+const uglier = require('./');
+const Config = require('./config');
+const globby = require('globby');
+const chalk = require('chalk');
+const _ = require('lodash');
+const mapLimit = require('../src/promises').mapLimit;
+const packageJSON = require('../package.json');
 
-/*
 
-uglify <script.js> -> code to stdout
-uglify
-
- */
-
-// var fname = "/users/Ray/webflow/lib/helpers/util.js";
-// "/users/Ray/webflow/models/dynamoDrivers/mongodb.js";
-var fname = "/users/Ray/webflow/lib/dnsUtils.js";
+const FILE_PARALLELISM = 16;
 
 var origAST = null, newAST = null;
 
-parser.parseFile(fname)
-  .then(saveOrigAST)
-  .then(astHandler.gurgitateAST)
-  .then(codez => formatter.mangle(codez, {}))
-  .then(saveNewAST)
-  .then(uglycode => console.log(uglycode))
-  .then(findASTDiffs)
-  .catch(err => {
-    console.error("ERROR: ", err.stack || String(err));
-  });
+function main(args) {
+  var c = new Config()
+    .loadPackage(packageJSON)
+    .loadArgv(args);
 
-function saveOrigAST(ast) {
-  origAST = ast;
-  return ast;
+  if (!c.patterns.length) {
+    console.log("No patterns given");
+    printUsage();
+    return Promise.resolve(1);
+  }
+
+  if (c.special === "help") {
+    printUsage();
+    return Promise.resolve(0);
+  }
+
+  if (c.special === "version") {
+    console.log(packageJSON.version);
+    return Promise.resolve(0);
+  }
+
+  console.log(c);
+
+  return loadPatterns(c.patterns)
+    .then(mangleWith(c));
 }
 
-function saveNewAST(code) {
-  newAST = parser.parseJS(code);
-  return code;
+function printUsage() {
+  console.log([
+    //                                                                             |
+    "Usage: uglier [options...] <glob> ...",
+    "Options:",
+    "  -o --overwrite  Overwrite files with uglier (but equivalent) code",
+    "  -p --print      Print results without changing them",
+    "  -h --help       Print this help message and exit",
+    "  -v --version    Print the version and exit",
+    "",
+    "Glob format is defined by the globby library. In summary:",
+    "  *               Any part of the path except for '/' characters",
+    "  **              Any part of the path including '/' characters, except for the final",
+    "                  part of the path",
+    "  ?               Any single character, except '/'",
+    "  !pattern        Will negate the pattern, so it matches the OPPOSITE of what was",
+    "                  described.",
+    "  {pattern,...}   Any one of the given patterns",
+  ].join("\n"));
 }
 
-function findASTDiffs() {
-  var delta = jsondiffpatch.diff(
-    _normalizeAST(origAST),
-    _normalizeAST(newAST)
-  );
-  if (!delta) { return; }
-  jsondiffpatch.console.log(delta);
+function loadPatterns(patterns) {
+  return globby(patterns);
 }
 
-function _normalizeAST(ast) {
-  ast = JSON.parse(JSON.stringify(ast));
-  return _.cloneDeepWith(ast, (val, key) => {
-    if (val && _.isObject(val)) {
-      delete val.comments;
-      delete val.leadingComments;
-      delete val.trailingComments;
-      delete val.loc;
-      delete val.start;
-      delete val.end;
-      delete val.tokens;
-      delete val.extra;
-    }
-  });
+function mangleWith(config) {
+  var onCode = printTrash;
+  var onError = printError;
+
+  return (files) => mapLimit(files, FILE_PARALLELISM, path => trashIt(path, onCode, onError));
+}
+
+function trashIt(path, onCode, onError) {
+  return uglier.mangleFile(path, { width: 80 })
+    .then(code => onCode(path, code))
+    .catch(err => onError(path, err));
+}
+
+/**
+ * Console methods:
+ */
+function printTrash(path, code) {
+  console.log('\n');
+  console.log(chalk.inverse("// %s"), _.padEnd(path, 77, " "));
+  console.log(code);
+}
+
+function printError(path, err) {
+  console.log('\n');
+  console.log(chalk.yellow.inverse("// %s"), _.padEnd(path, 77, " "));
+  console.log(err.message);
+}
+
+/**
+ * Overwrite methods:
+ */
+
+// ...
+
+
+if (require.main === module) {
+  main(process.argv)
+    .then(code => process.exit(code));
 }
