c056c23282f8c3c6a0a1e18ff7a8d6fb60ae629f
Ray Myers <raydog87@gmail.com>
2017-05-19 19:07:03 -0700

    Further work on main method

--- src/main.js
+++ src/main.js
@@ -32,8 +32,6 @@ function main(args) {
     return Promise.resolve(0);
   }
 
-  console.log(c);
-
   return loadPatterns(c.patterns)
     .then(mangleWith(c));
 }
@@ -64,39 +62,102 @@ function loadPatterns(patterns) {
 }
 
 function mangleWith(config) {
-  var onCode = printTrash;
-  var onError = printError;
-
-  return (files) => mapLimit(files, FILE_PARALLELISM, path => trashIt(path, onCode, onError));
+  return (files) => {
+    var handler = new TrashWriter(files);
+    return mapLimit(files, FILE_PARALLELISM, path => trashFile(path, handler))
+      .then(() => handler.onDone());
+  }
 }
 
-function trashIt(path, onCode, onError) {
+function trashFile(path, handler) {
   return uglier.mangleFile(path, { width: 80 })
-    .then(code => onCode(path, code))
-    .catch(err => onError(path, err));
+    .then(code => handler.onCode(path, code))
+    .catch(err => handler.onError(path, err));
 }
+
 
 /**
  * Console methods:
  */
-function printTrash(path, code) {
-  console.log('\n');
-  console.log(chalk.inverse("// %s"), _.padEnd(path, 77, " "));
-  console.log(code);
-}
+class TrashPrinter {
+  constructor(paths) {
+    this.ok = 0;
+    this.fail = 0;
+    this.total = paths.length;
+  }
 
-function printError(path, err) {
-  console.log('\n');
-  console.log(chalk.yellow.inverse("// %s"), _.padEnd(path, 77, " "));
-  console.log(err.message);
+  onCode(path, code) {
+    this.ok++;
+    console.log('\n');
+    console.log(chalk.inverse("// %s"), _.padEnd(path, 77, " "));
+    console.log(code);
+  }
+
+  onError(path, err) {
+    this.fail++;
+    console.log('\n');
+    console.log(chalk.yellow.inverse("// %s"), _.padEnd(path, 77, " "));
+    console.log(err.message);
+  }
+
+  onDone() {
+    var barLine = _.padEnd("// ", 80, "=");
+    var plural = this.total === 1 ? "file" : "files";
+    console.log('\n');
+    console.log(barLine);
+    console.log("// %d total %s: %d ok. %d fail", this.total, plural, this.ok, this.fail);
+    console.log(barLine);
+  }
 }
 
 /**
  * Overwrite methods:
  */
+const STATUS_FREQ = 50; // No more than 1 message per 50 ms.
+class TrashWriter {
+  constructor(paths) {
+    this.ok = 0;
+    this.fail = 0;
+    this.last = 0;
+    this.total = paths.length;
+    this.errors = [];
+  }
 
-// ...
+  _printStatus() {
+    var t = Date.now();
+    if (t - this.last < STATUS_FREQ) { return; }
+    this.last = t;
+    var pluralFile = this.total === 1 ? "file" : "files";
+    var pluralFail = this.fail === 1 ? "failure" : "failures";
+    var line = `${this.ok} / ${this.total} ${pluralFile} ok, ${this.fail} ${pluralFail}`;
+    process.stdout.write('\r  ' + _.padEnd(line, 78, " "));
+  }
 
+  onCode(path, code) {
+    this.ok++;
+    this._printStatus();
+  }
+
+  onError(path, err) {
+    this.fail++;
+    this.errors.push({ path, err });
+    this._printStatus();
+  }
+
+  onDone() {
+    var barLine = _.padEnd("// ", 80, "=");
+    var pluralFile = this.total === 1 ? "file" : "files";
+
+    console.log('\r' + _.repeat(' ', 80));
+    
+    this.errors.forEach(item => {
+      console.log(chalk.yellow.inverse("%s"), _.padEnd(item.path, 77, " "));
+      console.log(item.err.message);
+    });
+
+    console.log("Done! %d total %s: %d ok. %d fail", this.total, pluralFile, this.ok, this.fail);
+  }
+}
 
 if (require.main === module) {
   main(process.argv)
