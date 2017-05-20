e3c7d663a9fb90026e676117eda355b6b901803b
Ray Myers <raydog87@gmail.com>
2017-05-19 20:04:04 -0700

    Add support for minimum spacing between tokens

--- src/formatter.js
+++ src/formatter.js
@@ -5,7 +5,7 @@ exports.mangle = mangle;
 
 
 function mangle(codez, conf) {
-  var conf = _.defaults(conf, { width: 100 });
+  var conf = _.defaults(conf, { width: 100, minSpace: 3 });
   return codez
     .map(line => fuckUpLine(line, conf))
     .join("\n");
@@ -14,14 +14,16 @@ function mangle(codez, conf) {
 function fuckUpLine(line, conf) {
   var lines = [];
   var width = conf.width;
+  var minSpace = conf.minSpace;
   var indent = "";
   while (line.length) {
-    // Keep adding tokens until those tokens can no longer fit on a line with the minimum (1) char spacing:
-    for (var i=1; i<=line.length && _canFit(line.slice(0,i), width); i++) {
+    var newConf = { width, minSpace };
+    // Keep adding tokens until those tokens can no longer fit on a line with the current char spacing:
+    for (var i=1; i<=line.length && _canFit(line.slice(0,i), newConf); i++) {
       // pass
     }
     i = Math.max(1, i-1);
-    lines.push(indent + _spaceOut(line.slice(0, i), width));
+    lines.push(indent + _spaceOut(line.slice(0, i), newConf));
     line = line.slice(i);
     indent = "    ";
     width = conf.width - indent.length;
@@ -29,17 +31,23 @@ function fuckUpLine(line, conf) {
   return lines.join("\n");
 }
 
-function _canFit(line, width) {
-  if (!isFinite(width)) { return true; }
-  var flat = _flatten(line);
+function _canFit(line, conf) {
+  if (!isFinite(conf.width)) { return true; }
+  var flat = _flatten(line, conf);
+  var spaceCount = Math.max(0, flat.length-1) * conf.minSpace;
   var wc = _wordChars(flat);
-  return wc <= width;
+  return wc + spaceCount <= conf.width;
 }
 
 // Not only flattens a line, but will add spaces to the ends of tokens that
 // should be kept separate:
-function _flatten(line) {
+function _flatten(line, conf) {
   var flat = _.flattenDeep(line);
+  
+  // If there's some minimum on spacing, just depend on that:
+  if (conf.minSpace) { return flat; }
+  
+  // Else, there's no minimum, so we need to add spaces to some situations:
   for (var i=0; i<flat.length-1; i++) {
     var left = flat[i];
     var right = flat[i+1];
@@ -66,8 +74,9 @@ function _wordChars(flat) {
   return _.sumBy(flat, "length");
 }
 
-function _spaceOut(line, width) {
-  var flat = _flatten(line);
+function _spaceOut(line, conf) {
+  var flat = _flatten(line, conf);
+  var sc = Math.max(0, flat.length-1) * conf.minSpace;
   var wc = _wordChars(flat);
   var spaces = flat.length - 1;
 
@@ -75,11 +84,11 @@ function _spaceOut(line, width) {
   // we'll just cut our losses, and join with minimal spacing. This is to
   // handle situations where a large number of unsplittable tokens are in a
   // narrow width scenario...
-  if (wc >= width || !isFinite(width)) {
-    return flat.join("");
+  if (wc + sc >= conf.width || !isFinite(conf.width)) {
+    return flat.join(_.repeat(" ", conf.minSpace));
   }
 
-  var remain = width - wc;
+  var remain = conf.width - wc;
   var each = Math.floor(remain / spaces);
   var mod = remain % spaces;
 
