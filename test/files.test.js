3b49778d30725930da4cd0718a5dfccf72bb2cb8
Ray Myers <raydog87@gmail.com>
2017-05-26 10:42:53 -0700

    Add tests for promisey fs stuff

---
+++ test/files.test.js
@@ -0,0 +1,33 @@
+const test = require('ava').test;
+const files = require('../src/files');
+
+
+test("Can read from file", function (t) {
+  return files.readFile(__filename)
+    .then(data => {
+      t.regex(data, /THIS_RE_WILL_BE_IN_THERE/);
+    });
+});
+
+test("Errors on bad reads", function (t) {
+  return files.readFile("./this/is/not/a/real/file.js")
+    .catch(ex => {
+      t.regex(ex.message, /no such file or directory/i);
+    });
+});
+
+// TODO: File writing...
+
+test("Errors on bad writes", function (t) {
+  return files.writeFile("./this/is/not/a/real/file.js", "LOL")
+    .catch(ex => {
+      t.regex(ex.message, /no such file or directory/i);
+    });
+});
+
+test("Errors on bad mkdirs", function (t) {
+  return files.mkdir("./this/directory/cannot/be/created")
+    .catch(ex => {
+      t.regex(ex.message, /no such file or directory/i);
+    });
+});
